6f86ef32d5f8a1522ae722f0bfedbaf84711cbdd
Nerea Soler Benavent <nereamarisa56@gmail.com>
2022-09-28 22:31:44 +0200

    finish

--- src/main/java/net/ausiasmarch/wildcart/api/CalculatorController.java
+++ src/main/java/net/ausiasmarch/wildcart/api/CalculatorController.java
@@ -20,4 +20,29 @@ public class CalculatorController {
         oCalc.setResult(op1+op2);
         return new ResponseEntity<>(oCalc, HttpStatus.OK);
     }
+
+    @GetMapping("/minus/{op1}/{op2}")
+    public ResponseEntity<Calculation> minus(@PathVariable(value = "op1") int op1,
+                                            @PathVariable(value = "op2") int op2) {
+        Calculation oCalc = new Calculation(op1, op2);
+        oCalc.setResult(op1-op2);
+        return new ResponseEntity<>(oCalc, HttpStatus.OK);
+    }
+
+    @GetMapping("/multiply/{op1}/{op2}")
+    public ResponseEntity<Calculation> multiply(@PathVariable(value = "op1") int op1,
+                                        @PathVariable(value = "op2") int op2) {
+        Calculation oCalc = new Calculation(op1, op2);
+        oCalc.setResult(op1*op2);
+        return new ResponseEntity<>(oCalc, HttpStatus.OK);
+    }
+
+    @GetMapping("/div/{op1}/{op2}")
+    public ResponseEntity<Calculation> div(@PathVariable(value = "op1") int op1,
+                                        @PathVariable(value = "op2") int op2) {
+       Calculation oCalc = new Calculation(op1, op2);
+        oCalc.setResult(op1/op2);
+        return new ResponseEntity<>(oCalc, HttpStatus.OK);
+
+    }
 }
