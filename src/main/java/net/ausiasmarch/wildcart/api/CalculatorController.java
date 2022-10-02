e9d019998d1b741e76a0212d768716a253168864
Nerea Soler Benavent <nereamarisa56@gmail.com>
2022-10-02 23:27:32 +0200

    arreglao en intellij

--- src/main/java/net/ausiasmarch/wildcart/api/CalculatorController.java
+++ src/main/java/net/ausiasmarch/wildcart/api/CalculatorController.java
@@ -4,50 +4,15 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 /*import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;*/
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import net.ausiasmarch.wildcart.entity.Calculation;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 
 
 @RestController
+@CrossOrigin(origins="http://localhost:80")
 @RequestMapping("/calculator")
 public class CalculatorController {
-    /*
-    @GetMapping("/add/{op1}/{op2}")
-    public ResponseEntity<Calculation> add(@PathVariable(value = "op1") int op1,
-                                            @PathVariable(value = "op2") int op2) {
-        Calculation oCalc = new Calculation(op1, op2);
-        oCalc.setResult(op1+op2);
-        return new ResponseEntity<>(oCalc, HttpStatus.OK);
-    }
-
-    @GetMapping("/minus/{op1}/{op2}")
-    public ResponseEntity<Calculation> minus(@PathVariable(value = "op1") int op1,
-                                            @PathVariable(value = "op2") int op2) {
-        Calculation oCalc = new Calculation(op1, op2);
-        oCalc.setResult(op1-op2);
-        return new ResponseEntity<>(oCalc, HttpStatus.OK);
-    }
-
-    @GetMapping("/multiply/{op1}/{op2}")
-    public ResponseEntity<Calculation> multiply(@PathVariable(value = "op1") int op1,
-                                        @PathVariable(value = "op2") int op2) {
-        Calculation oCalc = new Calculation(op1, op2);
-        oCalc.setResult(op1*op2);
-        return new ResponseEntity<>(oCalc, HttpStatus.OK);
-    }
-
-    @GetMapping("/div/{op1}/{op2}")
-    public ResponseEntity<Calculation> div(@PathVariable(value = "op1") int op1,
-                                        @PathVariable(value = "op2") int op2) {
-       Calculation oCalc = new Calculation(op1, op2);
-        oCalc.setResult(op1/op2);
-        return new ResponseEntity<>(oCalc, HttpStatus.OK);
-
-    }*/
 
     @PostMapping ("/add")
     public ResponseEntity<Calculation> calculadoraA(@RequestBody Calculation oCalc) {
